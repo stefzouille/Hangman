e3ffdb8c0b5d51d332c3a2072493cdd52c18f5ea
stefzouille <contact.lokcy@gmail.com>
2021-04-07 18:44:03 +0200

    add

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ const startGame = () => {
     const { randomInt } = require('crypto')
 
     //pour test des mots rentré par user mettre ligne 11 en off et ligne 12 en on
-//declare ma variable avec le chemin de mon dico passer en fichier txt
-//const words = fs.readFileSync('./dico.txt', 'utf-8').split('\n')
+    //declare ma variable avec le chemin de mon dico passer en fichier txt
+    //const words = fs.readFileSync('./dico.txt', 'utf-8').split('\n')
    const words = ['pot', 'top']
-//fait un random de mon fichier txt dico
+    //fait un random de mon fichier txt dico
    const n = randomInt(0, words.length)
-//variable word du random passé en tab
+    //variable word du random passé en tab
    let word = words[n].split('')
 
    //ajoute les _ a la place des lettres
@@ -35,14 +35,13 @@ const startGame = () => {
       const index = readlineSync.question(chalk.blue(`ta fini de faire mumuse ? bon, press enter et continue oki...     `))
     } 
 
-// condition qui passe le tout en string car en tab refuse de sortir de la boucle...
+    // condition qui passe le tout en string car en tab refuse de sortir de la boucle...
     if (word.join('') === secret.join('')) {
-        console.log('c est la fin de ta life...')
+      console.log(word.join())
+        console.log(chalk.green('Bravo ,c est la fin de ta life...tu peu etre content..ou pas!'))
       process.exit(1)
     }
-}
-  
-  //word.find(letter => letter === input) ? secret[word.indexOf(input)] = input.replace() : console.log(' ')
+  }
 }
 startGame()
 
